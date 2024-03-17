50380fc8f62d393b4c84fd615a22c14bf33f95ce
studypankeev@gmail.com <stivenH0king@yandex.ru>
2024-03-17 19:19:03 +0400

    test write

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <vector>
+
+void writeMatrixToFile(const std::string &filename, const std::vector<std::vector<int>> &matrix)
+{
+    std::ofstream file(filename);
+
+    if (file.is_open())
+    {
+        for (const auto &row : matrix)
+        {
+            for (const int &element : row)
+            {
+                file << element << " ";
+            }
+            file << std::endl;
+        }
+
+        file.close();
+        std::cout << "Matrix has been written to file: " << filename << std::endl;
+    }
+    else
+    {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+    }
+}
 
 int main()
 {
-    std::cout << "World" << std::endl;
+    std::vector<std::vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    std::string filename = "matrix.txt";
+
+    writeMatrixToFile(filename, matrix);
+
+    return 0;
 }
